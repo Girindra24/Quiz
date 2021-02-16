c45e226a3756acb9a044f29f93dc93bc9e6643e5
unknown <girindrasai24@gmail.com>
2021-02-16 14:05:07 +0530

    Simple Quiz App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import questions from "./questions";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [score, setScore] = useState(0);
+	const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [showScore, setShowScore] = useState(false);
+	const [isReady, setIsReady] = useState(false);
+	const handleAnswerCheck = (validate) => {
+		console.log(`isCorrect --> ${validate}`);
+		if (validate) {
+			console.log(`Inside If`);
+			setScore(score + 1);
+		}
+		if (currentQuestion + 1 !== questions.length) {
+			setCurrentQuestion(currentQuestion + 1);
+		} else {
+			setShowScore(true);
+		}
+
+		console.log(`Score ${score} and currentQuestion ${currentQuestion}`);
+	};
+
+	return isReady ? (
+		showScore ? (
+			<div className='score-section'>
+				<p>You Scored {score}</p>
+			</div>
+		) : (
+			<div>
+				<div>
+					<p>Question: {currentQuestion + 1}</p>
+				</div>
+				<div>{questions[currentQuestion].question}</div>
+				<div>
+					{questions[currentQuestion].answerOptions.map((option) => {
+						return (
+							<div>
+								<button
+									onClick={() => {
+										handleAnswerCheck(option.isCorrect);
+									}}
+								>
+									{option.answerText}
+								</button>
+								{/* <label>{option.answerText}</label> */}
+								{/* <input
+									type='checkbox'
+									id='option'
+									name='option'
+									value='option'
+									defaultChecked={isChecking}
+									onClick={() => {
+										handleAnswerCheck(option.isCorrect);
+									}}
+								></input> */}
+							</div>
+						);
+					})}
+				</div>
+			</div>
+		)
+	) : (
+		<div className='welcome-section'>
+			<p>Welcome</p>
+			<button
+				onClick={() => {
+					setIsReady(true);
+				}}
+			>
+				Start the Quiz
+			</button>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/questions.js
@@ -0,0 +1,40 @@
+const questions = [
+	{
+		question: "How many days do we have in a week?",
+		answerOptions: [
+			{ answerText: "8", isCorrect: false },
+			{ answerText: "6", isCorrect: false },
+			{ answerText: "7", isCorrect: true },
+			{ answerText: "5", isCorrect: false },
+		],
+	},
+	{
+		question: "How many days are there in a non leap year?",
+		answerOptions: [
+			{ answerText: "369", isCorrect: false },
+			{ answerText: "365", isCorrect: true },
+			{ answerText: "363", isCorrect: false },
+			{ answerText: "362", isCorrect: false },
+		],
+	},
+	{
+		question: "Which animal is known as the ‘Ship of the Desert?’",
+		answerOptions: [
+			{ answerText: "Camel", isCorrect: true },
+			{ answerText: "Horse", isCorrect: false },
+			{ answerText: "Donkey", isCorrect: false },
+			{ answerText: "Elephant", isCorrect: false },
+		],
+	},
+	{
+		question: "How many consonants are there in the English alphabet?",
+		answerOptions: [
+			{ answerText: "22", isCorrect: false },
+			{ answerText: "20", isCorrect: false },
+			{ answerText: "23", isCorrect: false },
+			{ answerText: "21", isCorrect: true },
+		],
+	},
+];
+
+export default questions;
